71050ed0db9f9996ef5af1f1261a63d6b2bfa1f1
unsupervised-machine <taran.s.lau@gmail.com>
2023-11-08 00:09:20 -0800

    now updates and adds summoners who appear in searches to database

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from werkzeug.local import LocalProxy
 from flask_pymongo import PyMongo
 from models import User
 
+from datetime import datetime
+
 import requests
 
 from configparser import ConfigParser
@@ -58,13 +60,12 @@ def post_user():
 
 """
 TODO:
-    Functionality for user to search for by summoner name.
-    Display summoner_name, summoner_level and profile_icon_id (swap to image later) on webpage
-    Add puuid, summoner_name, profile_icon_id, and summoner_level to database
+    When someone searches for a summoner, update that summoners info in our database, if it doesnt exist add it to database
 """
 
 # instead of just printing raw json let's put the output in a nice template
 # just find and display summoner
+# instead o
 @app.route('/summoners/search', methods=["GET", "POST"])
 def summoner_search():
     # example usage: http://127.0.0.1:5000/summoners/search --> taran
@@ -77,37 +78,23 @@ def summoner_search():
         response = requests.get(api_url)
         if response.status_code == 200:
             data = response.json()
+            filter_criteria = {'puuid': data['puuid']}
+            update_operation = {
+                '$set': {
+                    'puuid': data['puuid'],
+                    'summoner_name': data['name'],
+                    'profile_icon_id': data['profileIconId'],
+                    'summoner_level': data['summonerLevel'],
+                    'last_updated': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                }
+            }
+            mongo.db.users.update_one(filter_criteria, update_operation, upsert=True)
             return render_template('display_summoner.html', data=data, the_title=data['name'])
         else:
             # Handle API error response
             error_message = "Error fetching summoner data"
             return {"error": error_message}, response.status_code
     return render_template('test_form.html', form=form)
-
-
-
-
-
-
-# find display and save summoner
-# @app.route('/summoners/search', methods=["GET", "POST"])
-# def summoner_search():
-#     form = SearchForSummoner()
-#     if form.validate_on_submit():
-#         summoner = {
-#             "summoner_name": form.summoner_name.data
-#         }
-#         api_url = f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner["summoner_name"]}?api_key={API_KEY}'
-#         data = requests.get(api_url).json()
-#     user = User(
-#         puuid=request.form["puuid"],
-#         summoner_name=request.form["summoner_name"],
-#         profile_icon_id=int(request.form["profile_icon_id"]),
-#         summoner_level=int(request.form["summoner_level"]),
-#     )
-#     mongo.db.users.insert_one({str(user)})
-#     return str(user)
-#
 
 
 
